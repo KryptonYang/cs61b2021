b02413776904fa304ea6e4b04ec1b6edd7b97614
Ke <krypton.yang@gmail.com>
2021-03-17 14:24:52 -0400

    complete lab2

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -14,6 +14,7 @@ public class IntListExercises {
             head.first += c;
             head = head.rest;
         }
+        head.first += c;
     }
 
     /**
@@ -51,7 +52,7 @@ public class IntListExercises {
      */
     public static boolean firstDigitEqualsLastDigit(int x) {
         int lastDigit = x % 10;
-        while (x > 10) {
+        while (x >= 10) {
             x = x / 10;
         }
         int firstDigit = x % 10;
@@ -67,16 +68,21 @@ public class IntListExercises {
      */
     public static boolean squarePrimes(IntList lst) {
         // Base Case: we have reached the end of the list
-        if (lst == null) {
-            return false;
+        boolean changed = false;
+//        if (lst == null) {
+//            return false;
+//        }
+        while (lst != null) {
+            boolean currElemIsPrime = Primes.isPrime(lst.first);
+            if (currElemIsPrime) {
+                lst.first *= lst.first;
+            }
+            lst = lst.rest;
+            changed = true;
         }
+        return changed;
 
-        boolean currElemIsPrime = Primes.isPrime(lst.first);
 
-        if (currElemIsPrime) {
-            lst.first *= lst.first;
-        }
-
-        return currElemIsPrime || squarePrimes(lst.rest);
+        //return currElemIsPrime || squarePrimes(lst.rest);
     }
 }
--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -10,11 +10,17 @@ public class SquarePrimesTest {
      * It passes, but the starter code implementation of isPrime
      * is broken. Write your own JUnit Test to try to uncover the bug!
      */
+
     @Test
     public void testSquarePrimesSimple() {
-        IntList lst = IntList.of(14, 15, 16, 17, 18);
-        boolean changed = IntListExercises.squarePrimes(lst);
-        assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
-        assertTrue(changed);
+//        IntList lst = IntList.of(14, 15, 16, 17, 18);
+//        boolean changed = IntListExercises.squarePrimes(lst);
+//        assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
+//        assertTrue(changed);
+
+        IntList lst1 = IntList.of(7, 9, 12, 13, 13);
+        boolean changed1 = IntListExercises.squarePrimes(lst1);
+        assertEquals("49 -> 9 -> 12 -> 169 -> 169", lst1.toString());
+        assertTrue(changed1);
     }
 }
